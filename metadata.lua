d33c34191c586178496cf2b6ffdc23c8668eecf2
野声 <artin@cat.ms>
2024-10-10 18:43:08 +0800

    Release 0.2.0

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "php"
 --- Plugin version
-PLUGIN.version = "0.1.1"
+PLUGIN.version = "0.2.0"
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/version-fox/vfox-php"
 --- Plugin license, please choose a correct license according to your needs.
